069c6716473644eaa60746c36d36246b6e7cc841
Marin <marin@stayke.net>
2017-01-09 16:05:12 -0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("/")
+		fmt.Fprintf(w, "hello world")
+	})
+
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
+}
