4703b3ce22369fc16ea67b32ae7d37f9ac4c483e
Marin <marin@stayke.net>
2017-01-13 10:43:12 -0800

    random sleep

--- main.go
+++ main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("/")
+		time.Sleep(rand.Int() % 3000 * time.Millisecond)
 		fmt.Fprintf(w, "hello world")
 	})
 
